9e3e7a4c51debfa61cca8ee002a9967a9a4c95c7
Faveero <159203076+Faveero@users.noreply.github.com>
2024-02-29 10:39:19 -0300

    p

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const botoes = document.querySelectorAll(".botao");
-for (let i = 0; i < botoes.lenght; i++) {
+for (let i = 0; i < botoes.length; i++) {
     botoes[i].onclick = function (){
         for (let j = 0; j < botoes.lenght; j++) {
             botoes[j].classList.remove("ativo");
