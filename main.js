7123ee8d19af3a3da21db79d2ca01a5efbd21607
Faveero <159203076+Faveero@users.noreply.github.com>
2024-02-29 10:30:45 -0300

    aaaaaa

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const botoes = document.querySelectorAll(".botao");
 for (let i = 0; i < botoes.lenght; i++) {
     botoes[i].onclick = function (){
         for (let j = 0; j < botoes.lenght; j++) {
-            botoes.classList.remove("ativo");
+            botoes[j].classList.remove("ativo");
         }
         botoes[i].classList.add("ativo");
     }
