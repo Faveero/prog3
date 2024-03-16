a1c9c357f79ff417daf804e8ad9d811f6fc34c2b
Faveero <159203076+Faveero@users.noreply.github.com>
2024-03-16 19:29:09 -0300

    cas

--- main.js
+++ main.js
@@ -8,5 +8,5 @@ for (let i = 0; i < botoes.length; i++) { //este é um loop que percorre cada bo
         }
         botoes[i].classList.add("ativo"); //depois de remover a classe 'ativo' de todos os botões, adicionamos a classe 'ativo' de volta ao botão que foi clicado. Isso efetivamente garante que apenas o botão clicado tenha a classe 'ativo'//
         textos[i].classList.add("ativo");        
-    };
+    }
 }
