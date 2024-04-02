9509ceca74b19a0a9689ce497c2dae2e0d254864
Faveero <159203076+Faveero@users.noreply.github.com>
2024-04-02 16:08:10 -0300

    iii

--- main.js
+++ main.js
@@ -12,5 +12,11 @@ for (let i = 0; i < botoes.length; i++) {
 }
 const contadores = document.querySelectorAll(".contador");
 const tempo1 = new Date("2024-12-11T00:00:00");
-let tempoAtual = new Date();
-contadores[0].textContent = tempo1 - tempoAtual;
+
+contadores[0].textContent = calculaTempo(tempo1);
+function calculaTempo(tempo){
+    let tempoAtual = new Date();
+    let tempoFinal = tempo - tempoAtual;
+    let segundos = tempoFinal / 1000;
+    return segundos;
+}
