6dde2495a69c2ff55f5c0de159fdac933c4b42de
Faveero <159203076+Faveero@users.noreply.github.com>
2024-04-02 16:02:45 -0300

    qw

--- main.js
+++ main.js
@@ -10,3 +10,7 @@ for (let i = 0; i < botoes.length; i++) {
         textos[i].classList.add("ativo");
     }
 }
+const contadores = document.querySelectorAll(".contador");
+const tempo1 = new Date("2024-12-11T00:00:00");
+let tempoAtual = new Date();
+contadores[0].textContent = tempo1 - tempoAtual;
