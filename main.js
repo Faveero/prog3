87a96dc175d7f16072bd6a0bab2bedb2634ead26
Faveero <159203076+Faveero@users.noreply.github.com>
2024-04-02 15:54:10 -0300

    be

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-const botoes = document.querySelectorAll(".botao"); //esta linha seleciona todos os elementos HTML que tem a classe 'botao' e armazena-os na constante 'botoes'//
+const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
-for (let i = 0; i < botoes.length; i++) { //este é um loop que percorre cada botao na constante 'botoes'//
-    botoes[i].onclick = function (){ //aqui, estamos atribuindo uma função ao evento 'onclick' de cada botão. Isso significa que a função será executada sempre que o botão for clicado//
-        for (let j = 0; j < botoes.length; j++) { //dentro da função 'onclick', temos outro loop que percorre cada botão novamente*/
-            botoes[j].classList.remove("ativo"); //aqui removemos a classe 'ativo' de cada botão. Isso pode ser usado para alterar o estilo do botão, por exemplo, para indicar qual botão está atualmente ativo//       
+for (let i = 0; i < botoes.length; i++) {
+    botoes[i].onclick = function (){
+        for (let j = 0; j < botoes.length; j++) {
+            botoes[j].classList.remove("ativo");        
             textos[j].classList.remove("ativo");    
         }
-        botoes[i].classList.add("ativo"); //depois de remover a classe 'ativo' de todos os botões, adicionamos a classe 'ativo' de volta ao botão que foi clicado. Isso efetivamente garante que apenas o botão clicado tenha a classe 'ativo'//      
+        botoes[i].classList.add("ativo");   
         textos[i].classList.add("ativo");
     }
 }
