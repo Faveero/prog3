f26e2251c827ea1135090f4dd8c843bceffdbc0a
Faveero <159203076+Faveero@users.noreply.github.com>
2024-04-03 10:04:44 -0300

    dv

--- main.js
+++ main.js
@@ -17,6 +17,14 @@ contadores[0].textContent = calculaTempo(tempo1);
 function calculaTempo(tempo){
     let tempoAtual = new Date();
     let tempoFinal = tempo - tempoAtual;
-    let segundos = tempoFinal / 1000;
-    return segundos;
+    let segundos = Math.floor(tempoFinal / 1000);
+    let minutos = Math.floor(segundos / 60);
+    let horas = Math.floor(minutos / 60);
+    let dias = Math.floor(horas / 24);
+
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
+
+    return dias + "dias" + horas + "horas" + minutos + "minutos" + segundos + "segundos";
 }
