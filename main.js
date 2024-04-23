6a8f8bdb6dfb28ddf6c0dbfac0d7292381e84de4
Faveero <159203076+Faveero@users.noreply.github.com>
2024-04-23 14:05:33 -0300

    cc

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ for (let i = 0; i < botoes.length; i++) { //laço de repetição que passará po
     }
 }
 const contadores = document.querySelectorAll(".contador"); //constante contador
-const tempo1 = new Date("2020-12-13T00:00:00"); //estipula uma data para a contagem regressiva
+const tempo1 = new Date("2024-12-13T00:00:00"); //estipula uma data para a contagem regressiva
 const tempo2 = new Date("2024-11-12T00:00:00");
 const tempo3 = new Date("2024-10-11T00:00:00");
 const tempo4 = new Date("2024-09-10T00:00:00");
